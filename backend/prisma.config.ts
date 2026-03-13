07c8cc5f4e4f4eae3b745388a375cde6cd0807bd
Tunji Akande <akandetunji3@gmail.com>
2026-03-13 12:59:39 +0100

    FEAT: updatessss 3

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL as string,
+    url: 'postgresql://task_master_user_db:5OOC8EqvLy0PVWgwnHwUcaSObStTvD6g@dpg-d6pna494tr6s7397tkng-a.oregon-postgres.render.com/task_master_db_txp4?sslmode=require',
   },
 });
